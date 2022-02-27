ae39f6d935d52d3f4be3d0caffc88f5f37b9c18a
Alex <chugun777akk@mail.ru>
2022-02-27 16:05:19 +0700

    New task

---
+++ Задача5/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine ("ВВедите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Последняя цифра это   ");
+Console.WriteLine(num1%10);
---
+++ задача4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("ВВедите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1%2==0)
+{
+    Console.WriteLine("Это число является четным");
+}
+else
+    Console.WriteLine("Это число не является четным");
